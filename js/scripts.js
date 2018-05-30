f4b0e22484d769bb69f555a218678cfb92befec7
Isachard Rodriguez <isachard@uoregon.edu>
2018-05-30 16:21:46 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiaW50ZXJuaXNhYyIsImEiOiJjamhnbnJuNHMxY25rMzlvN3FiaXo0N2RrIn0.aBs-W0QFuM8QYGcSCryRLA';
+
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style:'mapbox://styles/internisac/cjhs9p3tl054w2sqkxa3pfem0'
+});
